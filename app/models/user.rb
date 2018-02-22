cc635b48dfa32df6e85934f8f8724576954b048b
binyaha <yahabin@gmail.com>
2018-02-22 14:54:30 +0800

    heroku gogogo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :name
+
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
-  validates :name, uniqueness: true
+  
 
   mount_uploader :avatar, PhotoUploader
 
